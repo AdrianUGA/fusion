822dfdb8580256a984273fd20296ae296cced072
%axence Grand <maxencegrand1995@gmail.com>
2017-01-06 11:39:08 +0100

    etape 6

--- fusion.c
+++ fusion.c
@@ -14,7 +14,7 @@ void concatHeader(FILE * f1, FILE * f2, FILE * f3,Elf32_Ehdr * newHeader, Elf32_
 }
 
 /*Demander pour les attributs sh_link, sh_addr, sh_addralign, fonctionnement lors de la fusion*/
-void concatSectionHeader(Elf32_Ehdr * newHeader, Elf32_Ehdr * header, Elf32_Ehdr * tmp, char * sectionNames1[],char * sectionNames2[],Elf32_Shdr sectionHeaders1[], Elf32_Shdr sectionHeaders2[], FILE * f3){
+/*void concatSectionHeader(Elf32_Ehdr * newHeader, Elf32_Ehdr * header, Elf32_Ehdr * tmp, char * sectionNames1[],char * sectionNames2[],Elf32_Shdr sectionHeaders1[], Elf32_Shdr sectionHeaders2[], FILE * f3){
     int passe[header->e_shnum];
     int i,j,res=0;
     fseek(f3,sizeof(newHeader),SEEK_SET);
@@ -68,8 +68,49 @@ void concatSectionHeader(Elf32_Ehdr * newHeader, Elf32_Ehdr * header, Elf32_Ehdr
     fseek(f3,0,SEEK_SET);
     fread(&tmp,sizeof(Elf32_Ehdr),1,f3);
     tmp->e_shnum -= res;
-}
+}*/
 
+Elf32_Shdr * getSectionHeader(Elf32_Ehdr * header1, Elf32_Ehdr * header2, Elf32_Shdr sectionHeaders1[], Elf32_Shdr sectionHeaders2[], char * sectionNames1[], char * sectionNames2[],char** resChar ,int * size ){
+        Elf32_Shdr * res;
+        res = (Elf32_Shdr *)malloc(sizeof(Elf32_Shdr)*header1->e_shnum);
+        int i,l=1;
+        *size = 0;
+        for(i=0; i<header1->e_shnum; i++){
+            res[i] = sectionHeaders1[i];
+            *size=*size+1;
+            resChar[i] = (char *) malloc(sizeof(sectionNames1[i]));
+            resChar[i] = sectionNames1[i];
+
+        }
+        for(i=0; i<header2->e_shnum;i++){
+            if(sectionHeaders2[i].sh_type != SHT_PROGBITS){
+                res = realloc(res,l);
+                res[header1->e_shnum+(l-1)] = sectionHeaders2[i];
+                resChar[header1->e_shnum+(l-1)] = (char *) malloc(sizeof(sectionNames1[i]));
+                resChar[header1->e_shnum+(l-1)] = sectionNames2[i];
+                l++;
+                *size=*size+1;
+            } else {
+                int j, modif=1;
+                for(j=0; j<header1->e_shnum;j++){
+                    if(sectionNames1[j] == sectionNames2[i] && sectionHeaders1[j].sh_type == SHT_PROGBITS){
+                        modif = 0;
+                    }
+                }
+                if(modif){
+                    
+                    res = realloc(res,l);
+                    res[header1->e_shnum+(l-1)] = sectionHeaders2[i];
+                    res[header1->e_shnum+(l-1)] = sectionHeaders2[i];
+                    resChar[header1->e_shnum+(l-1)] = (char *) malloc(sizeof(sectionNames1[i]));
+                     resChar[header1->e_shnum+(l-1)] = sectionNames2[i];
+                    *size=*size+1;
+                    l++;
+                }
+            }
+        }
+        return res;
+}
 /*void concatSection(FILE * f1, FILE * f2, FILE * f3, char * sectionNames1[],char * sectionNames2[],Elf32_Shdr sectionHeaders1[], Elf32_Shdr sectionHeaders2[],Elf32_Ehdr * newHeader, Elf32_Ehdr * header){
     //Concatener les sections
     int i;
@@ -145,18 +186,23 @@ int main(int argc, char * argv[]){
     getSectionNames(f2, header, sectionHeaders2, sectionNames2);
     
     
-    concatHeader(f1,f2,f3,&newHeader, &header);
+    //concatHeader(f1,f2,f3,&newHeader, &header);
     
     
     Elf32_Ehdr tmp;
     fseek(f3,0,SEEK_SET);
     fread(&tmp, sizeof(tmp), 1,f3);
-    concatSectionHeader(&newHeader, &header, &tmp, sectionNames1, sectionNames2, sectionHeaders1,sectionHeaders2,f3);
+    //concatSectionHeader(&newHeader, &header, &tmp, sectionNames1, sectionNames2, sectionHeaders1,sectionHeaders2,f3);
+    int t=0, cpt = 0;
+    char * resChar[newHeader.e_shnum+header.e_shnum];
+    Elf32_Shdr * test = getSectionHeader(&newHeader, &header, sectionHeaders1, sectionHeaders2, sectionNames1, sectionNames2, resChar,&t);
+    printf("%d\n",t);
+    for(cpt;cpt<t;cpt++){
+            printf("%d et %d\n", test[cpt].sh_type,cpt);
+    }
     
-    
-    
-    Elf32_Ehdr header3;
+   /* Elf32_Ehdr header3;
     fseek(f3,0,SEEK_SET);
-    fread(&header3, sizeof(header),1, f3);
+    fread(&header3, sizeof(header),1, f3);*/
     //displayHeader(header3);
 }
