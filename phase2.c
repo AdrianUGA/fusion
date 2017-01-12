805da1c3fcad4274ce75e43f1f210897317e99bf
Adrian Amaglio <amaglioa@f202-04.e-im2ag.ujf-grenoble.fr>
2017-01-12 09:59:51 +0100

    no more segfault

--- phase2.c
+++ phase2.c
@@ -14,89 +14,27 @@
 #include "fusionelf.h"
 
 int main(int argc, char * argv[]){
+    if(argc < 4){
+        printf("To few arguments\n");
+        return -1;
+    }
+
+    add_debug_to("debug.log");
     elf_t elf1,elf2, elf3;
 
-    /* Existence et ouverture du fichier */
-    char *filename1;
-    char *filename2;
-    char *filename3;
+    initElf(&elf1, argv[1]);
+    initElf(&elf2, argv[2]);
 
-    filename1 = malloc(strlen(argv[1]) *  sizeof(char));
-    strcpy(filename1, argv[1]);
-    filename2 = malloc(strlen(argv[2]) *  sizeof(char));
-    strcpy(filename2, argv[2]);
+
+
+    char *filename3;
     filename3 = malloc(strlen(argv[3]) *  sizeof(char));
     strcpy(filename3, argv[3]);
-
-
-    elf1.file = fopen(filename1, "rb");
-    elf2.file = fopen(filename2, "rb");
     elf3.file = fopen(filename3, "wb+");
-
-
-    /* Lecture des ELF header */
-    int nbc = fread(&(elf1.header), 1, sizeof(elf1.header), elf1.file);
-    if(nbc != 1){
-        if(feof(elf1.file)){
-            /* End of file */
-        }else{
-            debug("Erreur de lecture.");
-        }
-    }
-
-    nbc = fread(&(elf2.header), 1, sizeof(elf2.header), elf2.file);
-    if(nbc != 1){
-        if(feof(elf2.file)){
-            /* End of file */
-        }else{
-            debug("Erreur de lecture.");
-        }
-    }
-
-    /* Vérification format ELF */
-    if(!(isElf(&elf1))){
-        fprintf(stderr, "Le fichier %s n'est pas au format ELF.\n", filename1);
-        return -1;
-    }
-
-    if(!(isElf(&elf2))){
-        fprintf(stderr, "Le fichier %s n'est pas au format ELF.\n", filename2);
-        return -1;
-    }
-
-    /* Récupération des Section headers */
-    elf1.sectionHeaders = malloc(elf1.header.e_shnum * sizeof(Elf32_Shdr));
-    fseek(elf1.file, elf1.header.e_shoff, SEEK_SET);
-    nbc = fread(elf1.sectionHeaders, elf1.header.e_shentsize, elf1.header.e_shnum, elf1.file);
-    if(nbc != 1){
-        if(feof(elf1.file)){
-            /* End of file */
-        }else{
-            debug("Erreur de lecture.");
-        }
-    }
-
-    elf2.sectionHeaders = malloc(elf2.header.e_shnum * sizeof(Elf32_Shdr));
-    fseek(elf2.file, elf2.header.e_shoff, SEEK_SET);
-    nbc = fread(elf2.sectionHeaders, elf2.header.e_shentsize, elf2.header.e_shnum, elf2.file);
-    if(nbc != 1){
-        if(feof(elf2.file)){
-            /* End of file */
-        }else{
-            debug("Erreur de lecture.");
-        }
-    }
-
-    /* Récupératuion des Section names */
-    elf1.sectionNames = malloc(elf1.header.e_shnum*sizeof(char*));
-    getSectionNames(&elf1);
-
-    elf2.sectionNames = malloc(elf2.header.e_shnum*sizeof(char*));
-    getSectionNames(&elf2);
 
     /* Récupération des tables de réalocation */
     getRelocTable(&elf1);
-    getRelocTable(&elf2);    
+    getRelocTable(&elf2);  
 
     /* Ecriture du fichier de fusion au format ELF */
     writeELF(elf1, elf2, &elf3);
