57e3cbcc58ded2d4af19d2c98d0640eb91fc5e5c
%axence Grand <maxencegrand1995@gmail.com>
2017-01-13 13:05:45 +0100

    test

--- test_display.sh
+++ test_display.sh
@@ -4,108 +4,166 @@
 
 
 #Fichier.o
-touch Test/display_header_obj.txt
-echo "Version readelf :" > Test/display_header_obj.txt
-readelf -h Test/file1.o >> Test/display_header_obj.txt
-echo " " >> Test/display_header_obj.txt
-echo "Version projet :" >> Test/display_header_obj.txt
-./main -h Test/file1.o >> Test/display_header_obj.txt
+touch Test/display_mult.txt
+echo "**************************header**************************" > Test/display_obj.txt
+echo "Version readelf :" >> Test/display_obj.txt
+readelf -h Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
+echo "Version projet :" >> Test/display_obj.txt
+./phase1 -h Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
 
-touch Test/display_section_header_obj.txt
-echo "Version readelf :" > Test/display_section_header_obj.txt
-readelf -S Test/file1.o >> Test/display_section_header_obj.txt
-echo " " >> Test/display_scetion_header_obj.txt
-echo "Version projet :" >> Test/display_section_header_obj.txt
-./main -S Test/file1.o >> Test/display_section_header_obj.txt
 
-touch Test/display_symbole_obj.txt
-echo "Version readelf :" > Test/display_symbole_obj.txt
-readelf -s Test/file1.o >> Test/display_symbole_obj.txt
-echo " " >> Test/display_symbole_obj.txt
-echo "Version projet :" >> Test/display_symbole_obj.txt
-./main -s Test/file1.o >> Test/display_symbole_obj.txt
+echo "**********************Section header**********************" >> Test/display_obj.txt
+echo "Version readelf :" >> Test/display_obj.txt
+readelf -S Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
+echo "Version projet :" >> Test/display_obj.txt
+./phase1 -S Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
 
-touch Test/display_realoc_obj.txt
-echo "Version readelf :" > Test/display_realoc_obj.txt
-readelf -r Test/file1.o >> Test/display_realoc_obj.txt
-echo " " >> Test/display_realoc_obj.txt
-echo "Version projet :" >> Test/display_realoc_obj.txt
-./main -r Test/file1.o >> Test/display_realoc_obj.txt
+
+echo "**************************Symbole**************************" >> Test/display_obj.txt
+echo "Version readelf :" >> Test/display_obj.txt
+readelf -s Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_mult.txt
+echo "Version projet :" >> Test/display_obj.txt
+./phase1 -s Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
+
+echo "*****************Table de réimplantation*****************" >> Test/display_obj.txt
+echo "Version readelf :" >> Test/display_obj.txt
+readelf -r Test/display/obj.o >> Test/display_obj.txt
+echo " " >> Test/display_obj.txt
+echo "Version projet :" >> Test/display_obj.txt
+./phase1 -r Test/display/obj.o >> Test/display_obj.txt
+
+
+
+
+
+#Fichier.o avec plusieurs table de réimplantations
+touch Test/display_mult.txt
+echo "**************************header**************************" > Test/display_mult.txt
+echo "Version readelf :" >> Test/display_mult.txt
+readelf -h Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+echo "Version projet :" >> Test/display_mult.txt
+./phase1 -h Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+
+
+echo "**********************Section header**********************" >> Test/display_mult.txt
+echo "Version readelf :" >> Test/display_mult.txt
+readelf -S Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+echo "Version projet :" >> Test/display_mult.txt
+./phase1 -S Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+
+
+echo "**************************Symbole**************************" >> Test/display_mult.txt
+echo "Version readelf :" >> Test/display_mult.txt
+readelf -s Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+echo "Version projet :" >> Test/display_mult.txt
+./phase1 -s Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+
+echo "*****************Table de réimplantation*****************" >> Test/display_mult.txt
+echo "Version readelf :" >> Test/display_mult.txt
+readelf -r Test/display/mult.o >> Test/display_mult.txt
+echo " " >> Test/display_mult.txt
+echo "Version projet :" >> Test/display_mult.txt
+./phase1 -r Test/display/mult.o >> Test/display_mult.txt
 
 
 
 
 
 #Fichier.o après fusion
-touch Test/display_header_fusion.txt
-echo "Version readelf :" > Test/display_header_fusion.txt
-readelf -h Test/file1.o >> Test/display_header_fusion.txt
-echo " " >> Test/display_header_fusion.txt
-echo "Version projet :" >> Test/display_header_fusion.txt
-./main -h Test/file1.o >> Test/display_header_fusion.txt
+touch Test/display_fusion.txt
+echo "**************************header**************************" > Test/display_fusion.txt
+echo "Version readelf :" >> Test/display_fusion.txt
+readelf -h Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
+echo "Version projet :" >> Test/display_fusion.txt
+./phase1 -h Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
 
-touch Test/display_section_header_fusion.txt
-echo "Version readelf :" > Test/display_section_header_fusion.txt
-readelf -S Test/fusion_reussi.o >>Test/display_section_header_fusion.txt
-echo " " >> Test/display_section_header_fusion.txt
-echo "Version projet :" >> Test/display_section_header_fusion.txt
-./main -S Test/fusion_reussi.o >> Test/display_section_header_fusion.txt
 
-touch Test/display_symbole_fusion.txt
-echo "Version readelf :" > Test/display_symbole_fusion.txt
-readelf -s Test/fusion_reussi.o >> Test/display_symbole_fusion.txt
-echo " " >> Test/display_symbole_obj.txt
-echo "Version projet :" >> Test/display_symbole_fusion.txt
-./main -s Test/fusion_reussi.o >> Test/display_symbole_fusion.txt
+echo "**********************Section header**********************" >> Test/display_fusion.txt
+echo "Version readelf :" >> Test/display_fusion.txt
+readelf -S Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
+echo "Version projet :" >> Test/display_fusion.txt
+./phase1 -S Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
 
-touch Test/display_realoc_fusion.txt
-echo "Version readelf :" > Test/display_realoc_fusion.txt
-readelf -r Test/fusion_reussi.o >> Test/display_realoc_fusion.txt
-echo " " >> Test/display_realoc_fusion.txt
-echo "Version projet :" >> Test/display_realoc_fusion.txt
-./main -r Test/fusion_reussi.o >> Test/display_realoc_fusion.txt
+
+echo "**************************Symbole**************************" >>Test/display_fusion.txt
+echo "Version readelf :" >> Test/display_fusion.txt
+readelf -s Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
+echo "Version projet :" >> Test/display_fusion.txt
+./phase1 -s Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
+
+echo "*****************Table de réimplantation*****************" >>Test/display_fusion.txt
+echo "Version readelf :" >> Test/display_fusion.txt
+readelf -r Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
+echo "Version projet :" >> Test/display_fusion.txt
+./phase1 -r Test/display/fusion.o >> Test/display_fusion.txt
+echo " " >> Test/display_fusion.txt
 
 
 
 
 
 #Fichier exec
-touch Test/display_header_exec.txt
-echo "Version readelf :" > Test/display_header_exec.txt
-readelf -h Test/fileEXEC >> Test/display_header_exec.txt
-echo " " >> Test/display_header_exec.txt
-echo "Version projet :" >> Test/display_header_exec.txt
-./main -h Test/fileEXEC >> Test/display_header_exec.txt
+touch Test/display_exec.txt
+echo "**************************header**************************" > Test/display_exec.txt
+echo "Version readelf :" >> Test/display_exec.txt
+readelf -h Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
+echo "Version projet :" >> Test/display_exec.txt
+./phase1 -h Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
 
-touch Test/display_section_header_obj.txt
-echo "Version readelf :" > Test/display_section_header_exec.txt
-readelf -S Test/fileEXEC >> Test/display_section_header_exec.txt
-echo " " >> Test/display_section_header_exec.txt
-echo "Version projet :" >> Test/display_section_header_exec.txt
-./main -S Test/fileEXEC >> Test/display_section_header_exec.txt
 
-touch Test/display_symbole_obj.txt
-echo "Version readelf :" > Test/display_symbole_exec.txt
-readelf -s Test/fileEXEC >> Test/display_symbole_exec.txt
-echo " " >> Test/display_symbole_exec.txt
-echo "Version projet :" >> Test/display_symbole_exec.txt
-./main -s Test/fileEXEC >> Test/display_symbole_exec.txt
+echo "**********************Section header**********************" >> Test/display_exec.txt
+echo "Version readelf :" >> Test/display_exec.txt
+readelf -S Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
+echo "Version projet :" >> Test/display_exec.txt
+./phase1 -S Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
 
-touch Test/display_realoc_obj.txt
-echo "Version readelf :" > Test/display_realoc_exec.txt
-readelf -r Test/fileEXEC >> Test/display_realoc_exec.txt
-echo " " >> Test/display_realoc_exec.txt
-echo "Version projet :" >> Test/display_realoc_exec.txt
-./main -r Test/fileEXEC >> Test/display_realoc_exec.txt
 
+echo "**************************Symbole**************************" >> Test/display_exec.txt
+echo "Version readelf :" >> Test/display_exec.txt
+readelf -s Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
+echo "Version projet :" >> Test/display_exec.txt
+./phase1 -s Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
+
+echo "*****************Table de réimplantation*****************" >> Test/display_exec.txt
+echo "Version readelf :" >> Test/display_exec.txt
+readelf -r Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
+echo "Version projet :" >> Test/display_exec.txt
+./phase1 -r Test/display/exec >> Test/display_exec.txt
+echo " " >> Test/display_exec.txt
 
 
 
 #Fichier non elf
 touch Test/display_notELF.txt
 echo "Version readelf :" > Test/display_notELF.txt
-readelf -h Test/file_non_ELF 2>> Test/display_notELF.txt
+readelf -h Test/display/non_elf.o 2>> Test/display_notELF.txt
 echo " " >> Test/display_notELF.txt
 echo "Version projet :" >> Test/display_notELF.txt
-./main -r Test/file_non_ELF 2>> Test/display_notELF.txt
+./phase1 -r Test/display/non_elf.o 2>> Test/display_notELF.txt
 
--- test_fusion.sh
+++ test_fusion.sh
@@ -3,51 +3,132 @@
 #fusion réussi
 touch Test/fusion_reussi.txt
 # ./fusion Test/file1.o Test/file2.o Test/test.o
-arm-none-eabi-ld -r -o Test/test.o Test/file1.o Test/prog.o
-echo "header" > fusion_reussi.txt
-echo "Version arm-none-eabi-" >> fusion_reussi.txt
-readelf -h Test/prog.o >> fusion_reussi.txt
-echo "Version ./fusion" >> fusion_reussi.txt
-readelf -h Test/test.o >> fusion_reussi.txt
-echo "" >> fusion_reussi.txt
-echo "section header" >> fusion_reussi.txt
-echo "Version arm-none-eabi-" >> fusion_reussi.txt
-readelf -S Test/prog.o >> fusion_reussi.txt
-echo "Version ./fusion" >> fusion_reussi.txt
-readelf -S Test/test.o >> fusion_reussi.txt
-echo "" >> fusion_reussi.txt
-echo "symbole" >> fusion_reussi.txt
-echo "Version arm-none-eabi-" >> fusion_reussi.txt
-readelf -s Test/prog.o >> fusion_reussi.txt
-echo "Version ./fusion" >> fusion_reussi.txt
-readelf -s Test/test.o >> fusion_reussi.txt
-echo "" >> fusion_reussi.txt
-echo "table des reimplantation" >> fusion_reussi.txt
-echo "Version arm-none-eabi-" >> fusion_reussi.txt
-readelf -r Test/prog.o >> fusion_reussi.txt
-echo "Version ./fusion" >> fusion_reussi.txt
-readelf -r Test/test.o >> fusion_reussi.txt
-echo "" >> fusion_reussi.txt
-echo "Génération de l'éxécutable et éxecution" >> fusion_reussi.txt
-echo "Version arm-none-eabi"
-arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/prog Test/prog.o && arm-none-eabi-run ./Test/prog 2>> fusion_reussi.txt
-echo "Version ./fusion" >> fusion.txt
-arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/test Test/test.o && arm-none-eabi-run ./Test/test 2>> fusion.txt
+arm-none-eabi-ld -r -o Test/fusion/fusion_eabi.o Test/fusion/file1.o Test/fusion/file2.o 2>error.txt
+./phase2 Test/fusion/file1.o Test/fusion/file2.o Test/fusion/fusion_projet.o 2>error.txt
+echo "******************************header******************************" > Test/fusion_reussi.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi.txt
+./phase1 -h Test/fusion/fusion_eabi.o >> Test/fusion_reussi.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+./phase1 -h Test/fusion/fusion_projet.o >> Test/fusion_reussi.txt
+echo "" >> Test/fusion_reussi.txt
+echo "******************************section header******************************" >> Test/fusion_reussi.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi.txt
+./phase1 -S Test/fusion/fusion_eabi.o >> Test/fusion_reussi.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+./phase1 -S Test/fusion/fusion_projet.o >> Test/fusion_reussi.txt
+echo "" >> Test/fusion_reussi.txt
+echo "******************************symbole******************************" >> Test/fusion_reussi.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi.txt
+./phase1 -s Test/fusion/fusion_eabi.o >> Test/fusion_reussi.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+./phase1 -s Test/fusion/fusion_projet.o >> Test/fusion_reussi.txt
+echo "" >> Test/fusion_reussi.txt
+echo "******************************table des reimplantation******************************" >> Test/fusion_reussi.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi.txt
+./phase1 -r Test/fusion/fusion_eabi.o >> Test/fusion_reussi.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+./phase1 -r Test/fusion/fusion_projet.o >> Test/fusion_reussi.txt
+echo "" >> Test/fusion_reussi.txt
+echo "******************************Génération de l'éxécutable et éxecution******************************" >> Test/fusion_reussi.txt
+echo "Version arm-none-eabi" >> Test/fusion_reussi.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/prog Test/fusion/fusion_eabi.o && arm-none-eabi-run ./Test/fusion/prog >> Test/fusion_reussi.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/test Test/fusion/fusion_projet.o && arm-none-eabi-run ./Test/fusion/test >> Test/fusion_reussi.txt
+
+
+
+#fusion réussi avec mult1.o
+touch Test/fusion_reussi_mult.txt
+# ./fusion Test/file1.o Test/file2.o Test/test.o
+arm-none-eabi-ld -r -o Test/fusion/fusion_eabi2.o Test/fusion/file2.o Test/fusion/mult1.o 2>error.txt
+./phase2 Test/fusion/file2.o Test/fusion/mult1.o Test/fusion/fusion_projet2.o 2>error.txt
+echo "******************************header******************************" > Test/fusion_reussi_mult.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult.txt
+./phase1 -h Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult.txt
+./phase1 -h Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult.txt
+echo "" >> Test/fusion_reussi_mult.txt
+echo "******************************section header******************************" >> Test/fusion_reussi_mult.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult.txt
+./phase1 -S Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult.txt
+./phase1 -S Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult.txt
+echo "" >> Test/fusion_reussi_mult.txt
+echo "******************************symbole******************************" >> Test/fusion_reussi_mult.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult.txt
+./phase1 -s Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult.txt
+./phase1 -s Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult.txt
+echo "" >> Test/fusion_reussi_mult.txt
+echo "******************************table des reimplantation******************************" >> Test/fusion_reussi_mult.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult.txt
+./phase1 -r Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult.txt
+./phase1 -r Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult.txt
+echo "" >> Test/fusion_reussi_mult.txt
+echo "******************************Génération de l'éxécutable et éxecution******************************" >> Test/fusion_reussi_mult.txt
+echo "Version arm-none-eabi" >> Test/fusion_reussi_mult.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/prog Test/fusion/fusion_eabi.o && arm-none-eabi-run ./Test/fusion/prog 2>> Test/fusion_reussi_mult.txt >> Test/fusion_reussi_mult.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/test Test/fusion/fusion_projet.o && arm-none-eabi-run ./Test/fusion/test 2>> Test/fusion_reussi._mult.txt >> Test/fusion_reussi_mult.txt
+
+
+
 
 
 
 #Fichier non translatable
 touch Test/fusion_non_translatable.txt
 echo "Version arm-none-eabi :" > Test/fusion_non_translatable.txt
-arm-none-eabi-ld -r -o Test/fusion.o Test/file2.o Test/fileEXEC 2>> Test/fusion_non_translatable.txt
-echo "Version ./fursion :"
-./fusion Test/file2.o Test/fileEXEC Test/test.o 2>> Test/fusion_non_translatable.txt
+arm-none-eabi-ld -r -o Test/fusion/fusion_nontranslatable_eabi.o Test/fusion/file2.o Test/fusion/exec 2>> Test/fusion_non_translatable.txt >> Test/fusion_non_translatable.txt
+echo "Version ./fusion :" >> Test/fusion_non_translatable.txt
+./phase2 Test/fusion/file2.o Test/fusion/exec Test/fusion/nontranslatable_projet.o 2>> Test/fusion_non_translatable.txt >> Test/fusion_non_translatable.txt
 
 
 
 #Fusion problèmes de symboles
 touch Test/fusion_echoue_symbole.txt
 echo "Version arm-none-eabi :" > Test/fusion_echoue_symbole.txt
-arm-none-eabi-ld -r -o Test/fusion.o Test/file1.o Test/file1.o 2>> Test/fusion_echoue_symbole.txt
-echo "Version ./fursion :"
-./fusion Test/file1.o Test/file1.o Test/test.o 2>> Test/fusion_echoue_symbole.txt
+arm-none-eabi-ld -r -o Test/fusion/echoue_eabi.o Test/fusion/file1.o Test/fusion/file1.o 2>> Test/fusion_echoue_symbole.txt
+echo "Version ./fusion :" >> Test/fusion_echoue_symbole.txt
+./phase2 Test/fusion/file1.o Test/fusion/file1.o Test/echoue_projet.o 2>> Test/fusion_echoue_symbole.txt
+
+
+
+
+
+
+#fusion réussi avec mult1.o et mult2.o
+touch Test/fusion_reussi_mult2.txt
+# ./fusion Test/file1.o Test/file2.o Test/test.o
+arm-none-eabi-ld -r -o Test/fusion/fusion_eabi2.o Test/fusion/mult2.o Test/fusion/mult1.o 2>error.txt
+./phase2 Test/fusion/mult2.o Test/fusion/mult1.o Test/fusion/fusion_projet2.o 2>error.txt
+echo "******************************header******************************" > Test/fusion_reussi_mult2.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult2.txt
+./phase1 -h Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult2.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult2.txt
+./phase1 -h Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult2.txt
+echo "" >> Test/fusion_reussi_mult2.txt
+echo "******************************section header******************************" >> Test/fusion_reussi_mult2.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult2.txt
+./phase1 -S Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult2.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult2.txt
+./phase1 -S Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult2.txt
+echo "" >> Test/fusion_reussi_mult2.txt
+echo "******************************symbole******************************" >> Test/fusion_reussi_mult2.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult2.txt
+./phase1 -s Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult2.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult2.txt
+./phase1 -s Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult2.txt
+echo "" >> Test/fusion_reussi_mult2.txt
+echo "******************************table des reimplantation******************************" >> Test/fusion_reussi_mult2.txt
+echo "Version arm-none-eabi-" >> Test/fusion_reussi_mult2.txt
+./phase1 -r Test/fusion/fusion_eabi2.o >> Test/fusion_reussi_mult2.txt
+echo "Version ./fusion" >> Test/fusion_reussi_mult2.txt
+./phase1 -r Test/fusion/fusion_projet2.o >> Test/fusion_reussi_mult2.txt
+echo "" >> Test/fusion_reussi_mult2.txt
+echo "******************************Génération de l'éxécutable et éxecution******************************" >> Test/fusion_reussi_mult2.txt
+echo "Version arm-none-eabi" >> Test/fusion_reussi_mult2.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/prog Test/fusion/fusion_eabi.o && arm-none-eabi-run ./Test/fusion/prog 2>> Test/fusion_reussi_mult2.txt >> Test/fusion_reussi_mul2t.txt
+echo "Version ./fusion" >> Test/fusion_reussi.txt
+arm-none-eabi-gcc --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -o Test/fusion/test Test/fusion/fusion_projet.o && arm-none-eabi-run ./Test/fusion/test 2>> Test/fusion_reussi._mult2.txt >> Test/fusion_reussi_mult2.txt
